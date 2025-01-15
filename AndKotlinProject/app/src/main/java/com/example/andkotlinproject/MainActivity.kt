a4801aa53e4564e0565a938ebdc7cf253787032a
Anchaeyeon <s2328@e-mirim.hs.kr>
2025-01-15 15:53:29 +0900

    메뉴 바 xml 완성

--- AndKotlinProject/app/src/main/java/com/example/andkotlinproject/MainActivity.kt
+++ AndKotlinProject/app/src/main/java/com/example/andkotlinproject/MainActivity.kt
@@ -1,12 +1,8 @@
 package com.example.andkotlinproject
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.andkotlinproject.databinding.ActivityMainBinding
-
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
