6fd048245a705ed2f030f2f983e2f163e8828868
Anchaeyeon <s2328@e-mirim.hs.kr>
2025-01-15 13:53:10 +0900

    프로젝트 생성 & 메인 xml 작성

---
+++ AndKotlinProject/app/src/main/java/com/example/andkotlinproject/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.andkotlinproject
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.andkotlinproject.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        /*  바인딩 사용 예   : bindig.id이름.XXX
+        binding.btn.setOnClickListener {
+            binding.txtResult.text = "계산결과"
+        }
+       */
+    }
+}
