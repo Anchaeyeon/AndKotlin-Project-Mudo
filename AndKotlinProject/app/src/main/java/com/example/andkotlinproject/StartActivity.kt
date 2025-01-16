11baa5d58791eb75049f267846d543ab7a2ec13e
Anchaeyeon <s2328@e-mirim.hs.kr>
2025-01-16 13:36:31 +0900

    파일 이름 변경

--- AndKotlinProject/app/src/main/java/com/example/andkotlinproject/StartActivity.kt
+++ AndKotlinProject/app/src/main/java/com/example/andkotlinproject/StartActivity.kt
@@ -4,13 +4,13 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
-import com.example.andkotlinproject.databinding.StartScreenBinding
+import com.example.andkotlinproject.databinding.ActivityStartBinding
 
 class StartActivity : AppCompatActivity() {
-    private lateinit var binding : StartScreenBinding
+    private lateinit var binding : ActivityStartBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = StartScreenBinding.inflate(layoutInflater)
+        binding = ActivityStartBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         Handler().postDelayed({
