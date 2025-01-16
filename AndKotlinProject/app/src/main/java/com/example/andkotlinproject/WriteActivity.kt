7ae5aa392f397180e36a45df64dc834c3f134861
Anchaeyeon <s2328@e-mirim.hs.kr>
2025-01-17 03:43:55 +0900

    뒤로가기 추가

--- AndKotlinProject/app/src/main/java/com/example/andkotlinproject/WriteActivity.kt
+++ AndKotlinProject/app/src/main/java/com/example/andkotlinproject/WriteActivity.kt
@@ -1,20 +1,29 @@
 package com.example.andkotlinproject
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.andkotlinproject.databinding.ActivityWriteBinding
 
 class WriteActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityWriteBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_write)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityWriteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 툴바 세팅
+        setSupportActionBar(binding.toolbar)
+
+        // Actionbar에서 뒤로가기 버튼 활성화
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setDisplayShowHomeEnabled(true)
         }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        finish()
+        onBackPressedDispatcher.onBackPressed() // 뒤로가기
+        return true
     }
 }
